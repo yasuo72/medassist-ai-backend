6499b5951fe530a70167a33d44c76bf779eb252e
Your Name <kingrohi37@gmail.com>
2025-06-15 15:32:40 +0530

    updated

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -19,4 +20,5 @@ def analyze_data():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
